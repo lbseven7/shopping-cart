7925bef80ea147460acadbba698707838ef38300
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-03-25 16:14:45 -0300

    Requisito 2 fetchItem.js ok

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aquii
+const fetchItem = async (id) => {
+  try {
+    const itemsApi = `https://api.mercadolibre.com/items/${id}`;
+  const response = await fetch(itemsApi);
+  return response.json();
+  } catch {
+    return new Error('You must provide an url'); 
+  }
+  
 };
 
 if (typeof module !== 'undefined') {
